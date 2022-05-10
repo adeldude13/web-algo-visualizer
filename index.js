5297c624d7bc563cd274f39c679d3884b0e07b8c
adeldude13 <adeldude13@protonmail.com>
2022-05-10 13:47:08 +0300

    Added Quick Sort

--- index.js
+++ index.js
@@ -45,8 +45,42 @@ const bubble = async (delay=500) => {
 	isSorting = false;
 }
 
-const sort = () => {
+
+const quick = async (delay) => {
+	let arr = document.querySelectorAll(".elem");
+	console.log(arr);
+	let j=0;
+	while(j < LENGTH-1) {
+		let i = j+1;
+		arr[i].style.backgroundColor = "green";
+		arr[i-1].style.backgroundColor = "green";
+		let tmp = i;
+		await sleep(delay);
+		while(i > 0 && arr[i-1].offsetHeight > arr[i].offsetHeight) {
+			arr[i].style.backgroundColor = "green";
+			arr[i-1].style.backgroundColor = "green";
+			let temp = arr[i].style.height;
+			arr[i].style.height = arr[i-1].style.height;
+			arr[i-1].style.height = temp;
+			await sleep(delay);
+			arr[i].style.backgroundColor = "red";
+			arr[i-1].style.backgroundColor= "red";
+			i-=1;
+		}
+		arr[tmp].style.backgroundColor = "red";
+		arr[tmp-1].style.backgroundColor = "red";
+		j+=1;
+	}
+}
+
+
+
+
+
+
+const sort = async () => {
 	// currenlty only apply bubble sort
+	let arr = document.querySelectorAll(".elem");
 	try {	
 		let delay = parseInt(input.value);
 		if(isNaN(delay)) {
